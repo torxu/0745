109a6a01fa46559af4faa337c95bc0e775f33060
torxu <aitoroterocarpintero@gmail.com>
2016-11-03 09:59:31 +0100

    0745.a Añadida funcionalidad de vaciar maquina y mostrar la cantidad que tenia

--- TicketMachine.java
+++ TicketMachine.java
@@ -99,4 +99,12 @@ public class TicketMachine
         balance = 0;
         return amountToRefund;
     }
+    
+    public int emptyMachine()
+    {
+        int amountToRefund;
+        amountToRefund = total;
+        total = 0;
+        return amountToRefund;
+    }
 }
